9454adad3b4a6ccd2f2a83d00a29f6aee0a228a7
Dony Wahyu Ramadhan <donyramadhan3011@gmail.com>
2025-09-15 06:22:45 +0700

    remove unused import

--- src/main/java/com/cvms/api/service/RankingServiceImpl.java
+++ src/main/java/com/cvms/api/service/RankingServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -19,21 +18,17 @@ import com.cvms.api.repository.CandidateRepository;
 import com.cvms.api.repository.VacancyRepository;
 import com.cvms.api.service.calculator.CriteriaCalculator;
 
-import lombok.AllArgsConstructor;
-
 @Service
 public class RankingServiceImpl implements RankingService {
     private final CandidateRepository candidateRepository;
     private final VacancyRepository vacancyRepository;
     private final Logger logger = Logger.getLogger(RankingServiceImpl.class.getName());
-    private final List<CriteriaCalculator> calculators;
     private final Map<CriteriaType, CriteriaCalculator> calculatorMap;
 
     public RankingServiceImpl(CandidateRepository candidateRepository, VacancyRepository vacancyRepository,
             List<CriteriaCalculator> calculators) {
         this.candidateRepository = candidateRepository;
         this.vacancyRepository = vacancyRepository;
-        this.calculators = calculators;
         this.calculatorMap = calculators.stream().collect(
                 Collectors.toMap(CriteriaCalculator::getType, calculator -> calculator));
     }
